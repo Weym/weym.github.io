5f15b8ad98c3244cf132bd770664f79426dcafd5
Weydson <weydsonmarinho@gmail.com>
2025-12-16 16:11:54 -0300

    add: js animation for the navbar menu

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+feather.replace();
+
+const toggleButton = document.querySelector('.center-circle');
+const menuList = document.querySelector('.circle-menu');
+
+toggleButton.addEventListener('click', () => {
+    // We toggle the class on the parent container
+    menuList.classList.toggle('menu-active');
+});
+
